422a1d72bfa690b64b3ad6571e65101999e2b37a
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-08-23 10:33:48 +0100

    Updated tests

--- test/cl.py
+++ test/cl.py
@@ -3,6 +3,23 @@ import cdhit_reader
 
 import os,sys
 
-if os.path.exists(sys.argv[1]):
-    for i in cdhit_reader.read_cdhit(sys.argv[1]):
-        print(i)
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(script_dir)
+input_clst = os.path.join(parent_dir, "data", "aa.clstr")
+
+
+if os.path.exists(input_clst):
+
+    total = 0
+    members = 0
+    for i in cdhit_reader.read_cdhit(input_clst):
+        total += 1
+        members += len(i)
+        print(i, file=sys.stderr)
+
+    
+    if total == 7 and members == 10:
+        print("OK: {} clusters from {} sequences".format(total, members), file=sys.stderr)
+    else:
+        print("FAIL: {} clusters from {} sequences (expected 7, 10)".format(total, members), file=sys.stderr)
+        sys.exit(1)
--- test/fa.py
+++ test/fa.py
@@ -2,7 +2,22 @@
 import cdhit_reader
 
 import os,sys
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(script_dir)
+input_fasta = os.path.join(parent_dir, "data", "test.fa.gz")
 
-if os.path.exists(sys.argv[1]):
-    for i in cdhit_reader.read_fasta(sys.argv[1], line_len=60):
-        print(i)
+if os.path.exists(input_fasta):
+    tot = 0
+    comments = 0
+    bp = 0
+    for i in cdhit_reader.read_fasta(input_fasta, line_len=60):
+        print("[seqname=", i.name, "|comment=", i.comment, "]", sep="",file=sys.stderr)
+        tot += 1
+        bp  += len(i)
+        if i.comment:
+            comments += 1
+    if tot == 3 and comments == 2 and bp == 29:
+        print("OK: {} sequences from {} comments, total {} bp".format(tot, comments, bp), file=sys.stderr)
+    else:
+        print("FAIL: {} sequences from {} comments,  total {} bp (expected 3, 2, 29)".format(tot, comments, bp), file=sys.stderr)
+        sys.exit(1)
